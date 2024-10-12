d1267da91bd1de76942ae2ead76a0919ec7347be
Christopher Vecchione <christopher.vecchione@onezerollc.com>
2024-10-12 19:49:04 -0400

    Setting up a mock api to troubleshoot issues

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,7 +3,8 @@ import { useState, useEffect } from "react";
 export const useFetch = (apiPath, queryTerm="") => {
     const [data, setData] = useState([]);
         
-    const url = `https://api.themoviedb.org/3/${apiPath}?language=en-US&page=1&query=${queryTerm}`
+    //const url = `https://api.themoviedb.org/3/${apiPath}?language=en-US&page=1&query=${queryTerm}`
+    const url = 'https://670b075cac6860a6c2cb0f06.mockapi.io/movie'
 
     useEffect(() => {
         async function fetchMovies(){
@@ -14,7 +15,8 @@ export const useFetch = (apiPath, queryTerm="") => {
                     Authorization: `Bearer ${process.env.TMDB_READ_ACCESS_TOKEN}`
                 }
             };
-            const response = await fetch(url, options);
+            //const response = await fetch(url, options);
+            const response = await fetch(url);
             const json = await response.json();
             setData(json.results);
         }
